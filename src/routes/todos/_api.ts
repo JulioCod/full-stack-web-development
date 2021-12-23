f9ec9d28216a43b234d7ccd0375799c69d241410
JulioCod <juliocodesal@gmail.com>
2021-12-23 18:16:32 +0000

    Enhance HTML forms.

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -18,25 +18,26 @@ export const api = (request: Request, data?: Record<string, unknown>) => {
             body = data;
             status = 201;
             break
-            case "DELETE":
-                todos = todos.filter(todo => todo.uid !== request.params.uid)
-                status = 200;
-                break   
-            case "PATCH":
-                todos = todos.map(todo => {
-                    if (todo.uid === request.params.uid) {
-                        if (data.text) todo.text = data.text as string;
-                        else todo.done = data.done as boolean;
-                    }
-                    return todo;
-                })
-                status = 200;
-                break   
+        case "DELETE":
+            todos = todos.filter(todo => todo.uid !== request.params.uid)
+            status = 200;
+            break   
+        case "PATCH":
+            todos = todos.map(todo => {
+                if (todo.uid === request.params.uid) {
+                    if (data.text) todo.text = data.text as string;
+                    else todo.done = data.done as boolean;
+                }
+                return todo;
+            })
+            status = 200;
+            body = todos.find(todo => todo.uid === request.params.uid);
+            break;
         default:
             break;
     }
 
-    if (request.method.toUpperCase() !== "GET") {
+    if (request.method.toUpperCase() !== "GET" && request.headers.accept !== "application/json") {
         return {
             status: 303,
             headers: {
